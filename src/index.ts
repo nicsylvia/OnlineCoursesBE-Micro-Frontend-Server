427c020b5c4caf41402375aa4f29e347b322a191
nicsylvia <nicsylvia15f@gmail.com>
2023-06-07 01:42:35 -0700

    saved conncetion

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express, { Application } from "express";
+import { MainAppConfig } from "./MainApp";
+import { DataBaseConnection } from "./Config/Database";
 
 const port: number = 1105;
 
 const app: Application = express();
+MainAppConfig(app);
+DataBaseConnection();
 
 app.listen(port, () => {
   console.log("");
